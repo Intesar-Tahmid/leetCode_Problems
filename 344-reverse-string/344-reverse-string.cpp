654b5397e1af8449069ab0d072057ed26fc8b15a
Intesar-Tahmid <68694638+Intesar-Tahmid@users.noreply.github.com>
2022-05-12 11:55:23 +0600

    Time: 45 ms (12.17%), Space: 23.1 MB (76.64%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        char temp;
+        for(int i = 0; i<n/2; i++)
+        {
+            temp = s[i];
+            s[i] = s[n-i-1];
+            s[n-i-1] = temp;
+           // swap(s[i], s[n-i-1]);
+        }
+        
+    }
+};
